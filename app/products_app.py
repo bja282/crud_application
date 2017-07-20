693545ff6d377cf4531381b4d284ea470a107e6a
Benjamin Alpert <bja282@nyu.edu>
2017-07-19 20:22:09 -0400

    Working on different functions.

--- app/products_app.py
+++ app/products_app.py
@@ -1,53 +1,82 @@
-# Checkpoint I - User Inputs
-#
-# 1. Print a menu which contains a greeting message and a hard-coded number of products and a list of available operations.
+import csv
+import code
+csv_file_path = "../data/products.csv"
 
-# 2.  Using the aforementioned menu, prompt the user to choose one of the available operations, and print the name of the chosen operation.
+products = []
 
-# 3. Implement a single "handler" function to recognize the chosen operation and invoke one of a handful of new operation-specific
-#functions to perform the chosen operation. For example, if the user chooses "Create", have your "handler" function invoke a function
-#called create_product() to print the name of the chosen operation.
-
-# 4. Handle invalid operation inputs by displaying a helpful message like "Unrecognized Operation. Please choose one of: 'List', 'Show', 'Create', 'Update', or 'Destroy'."
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        products.append(row)
 
 def list_products():
-    print("This is a list of all the products!")
+    with open(csv_file_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file) # assuming your CSV has headers, otherwise... csv.reader(csv_file)
+        for row in reader:
+            print(row["id"],row["name"],row["aisle"],row["department"],row["price"])
 
 def show_products():
-    print("This is showing all the products!")
+    print('''This is showing all the products!
+    \n
+    ---------------------------
+    \n''')
 
 def create_products():
-    print("Wow, you made a new product.")
+    with open(csv_file_path, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
+        writer.writeheader() # uses fieldnames set above
+        for product in products:
+            writer.writerow(product)
 
 def update_products():
-    print("Woahhhhh, Updates?!")
+    print('''Woahhhhh, Updates?!
+    \n
+    ---------------------------
+    \n''')
 
 def destroy_products():
-    print("You are a destroyer of worlds.")
+    print('''You are a destroyer of worlds.
+    \n
+    ---------------------------
+    \n''')
 
-def handler(input):
-    if input.lower()=="list":
+def handler(selection):
+    if selection=="list":
         return list_products()
-    elif input.lower()=="show":
+    elif selection=="show":
         return show_products()
-    elif input.lower()=="create":
+    elif selection=="create":
         return create_products()
-    elif input.lower()=="update":
+    elif selection=="update":
         return update_products()
-    elif input.lower()=="destroy":
+    elif selection=="destroy":
         return destroy_products()
     else:
-        print("Well that doesn't seem right. You sure?")
+        print("Well that doesn't seem right. You sure?\n\n")
 
-directions = ('''There are 20 products in the database. Please select an operation:
+directions = ('''There are {0} products in the database. Please select an operation:
 
 operation | description
 --------- | -----------
-'List'    | Display a list of product identifiers and namesself.
-'Show'    | Show information about a productself.
+'List'    | Display a list of product identifiers and names.
+'Show'    | Show information about a products.
 'Create'  | Add a new product.
 'Update'  | Edit an existing product.
-'Destroy' | Delete an existing product.''')
+'Destroy' | Delete an existing product.''').format(len(products))
 
-input = input(directions +  "\n\nPlease select an operation: ")
-handler(input)
+print(directions)
+
+x=0
+while x == 0:
+    user_input = input("\n\nPlease select an operation: ")
+    user_input = user_input.lower()
+    try:
+        if user_input.lower() == "done":
+            break
+        elif user_input != "done":
+            handler(user_input)
+    except ValueError:
+        print("Sorry, that input doesn't seem correct. Try again!")
+
+
+#code.interact(local=locals())
