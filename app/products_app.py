150b38ae1201afcb250b4395b9b4f04de1e6a05a
Benjamin Alpert <bja282@nyu.edu>
2017-07-21 16:04:34 -0400

    Updating file with destroy, update and show functions functional.

--- app/products_app.py
+++ app/products_app.py
@@ -9,18 +9,35 @@ with open(csv_file_path, "r") as csv_file:
     for row in reader:
         products.append(row)
 
+writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
+
+# The List operation should print information (identifiers and names at least) about each product
+# in the inventory.
 def list_products():
     with open(csv_file_path, "r") as csv_file:
         reader = csv.DictReader(csv_file) # assuming your CSV has headers, otherwise... csv.reader(csv_file)
         for row in reader:
             print(row["id"],row["name"],row["aisle"],row["department"],row["price"])
 
+# The Show operation should prompt the user for a product identifier. If the product
+# identifier matches the identifier of an existing product in the inventory, the program
+# should print all available information about that product.
 def show_products():
-    print('''This is showing all the products!
-    \n
-    ---------------------------
-    \n''')
+    product_key = input("Please enter the id of the product you would like to view: ")
+    if int(product_key) <= len(products):
+        first_q = input("You selected \"{0}".format(products[int(product_key)-1]["name"]) + "\". Is that correct? (Enter yes or no) ")
+        if first_q.lower() == "yes":
+            print("\n")
+            for key, value in products[int(product_key)-1].items():
+                print(str(key)+":"+str(value))
+        else: pass
+    else:
+        print("Sorry, that product key appears incorrect.")
 
+# The Create operation should prompt the user to input a new product's "name", "department", "aisle"
+# and "price", and should automatically determine the new product's "id" by adding 1 to the greatest
+# identifier currently in the inventory. Then the program should save the new product's information by
+# adding a new row at the bottom of the CSV file.
 def create_products():
     new_product = dict.fromkeys(["id", "name", "aisle", "department", "price"])
     new_id = len(products)+1
@@ -47,18 +64,42 @@ def create_products():
         reader = csv.DictReader(csv_file)
         products.append(new_product)
 
-
+# The Update operation should prompt the user for a product identifier.
+# If the product identifier matches the identifier of an existing product in the inventory,
+# the program should prompt the user to input new values for that product's "name", "department",
+# "aisle" and "price" attributes, and overwrite that product's corresponding row in the CSV file.
 def update_products():
-    print('''Woahhhhh, Updates?!
-    \n
-    ---------------------------
-    \n''')
+    product_key = input("Please enter the id of the product you would like to change: ")
+    if int(product_key) <= len(products):
+        first_q = input("You selected \"{0}".format(products[int(product_key)-1]["name"]) + "\". Is that correct? (Enter yes or no) ")
+        if first_q.lower() == "yes":
+            new_name = input("Please enter a product name: ")
+            new_aisle = input("Which aisle is the product in? ")
+            new_dept = input("Which department is the product in? ")
+            new_price = input("What is the price of the item? ")
+            products[int(product_key)-1]={
+            'id': product_key,
+            "department": new_dept,
+            "aisle": new_aisle,
+            "name": new_name,
+            "price": new_price}
+            print("Thank you,",new_name,"has been updated.")
 
+    else:
+        print('Sorry, that product key doesn\'t appear correct.')
+
+# The Destroy operation should prompt the user for a product identifier.
+# If the product identifier matches the identifier of an existing product in the inventory,
+# the program should display a helpful message and remove that product's corresponding row from the CSV file.
 def destroy_products():
-    print('''You are a destroyer of worlds.
-    \n
-    ---------------------------
-    \n''')
+    product_key = input("Please enter the id of the product you would like to delete: ")
+    if int(product_key) <= len(products):
+        first_q = input("You selected \"{0}".format(products[int(product_key)-1]["name"]) + "\". Is that correct? (Enter yes or no) ")
+        if first_q.lower() == "yes":
+            print("Thank you,",products[int(product_key)-1]["name"],"has been deleted.")
+            del products[int(product_key)-1]
+    else:
+        print('Sorry, that product key doesn\'t appear correct.')
 
 def handler(selection):
     if selection=="list":
@@ -82,12 +123,18 @@ operation | description
 'Show'    | Show information about a products.
 'Create'  | Add a new product.
 'Update'  | Edit an existing product.
-'Destroy' | Delete an existing product.''').format(len(products))
+'Destroy' | Delete an existing product.\n
+You may exit the program at any time by entering "done."''').format(len(products))
 
 print(directions)
 
 x=0
 while x == 0:
+    with open(csv_file_path, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
+        writer.writeheader() # uses fieldnames set above
+        for product in products:
+            writer.writerow(product)
     user_input = input("\n\nPlease select an operation: ")
     user_input = user_input.lower()
     try:
